c50f388855ef8ca19f1544da8d86afebbc7b3d46
krishee107 <krishee107@gmail.com>
2023-04-27 19:21:33 +0200

    Flatlist para hacer un carousel de películas y scrollview para deslizarse hacia el final

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { ActivityIndicator, Text, View } from 'react-native'
+import { FlatList, ScrollView, Text, View } from 'react-native'
 import Carousel from 'react-native-snap-carousel';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
@@ -24,15 +24,38 @@ export const HomeScreen = () => {
 
 
     return (
-        <View style={{ marginTop: top + 20 }}>
-            <View style={{ height: 440, }} >
-                <Carousel
-                    data={peliculasEnCine}
-                    renderItem={({ item }: any) => <MoviePoster movie={item} />}
-                    sliderWidth={windowWidth}
-                    itemWidth={300}
-                />
+        <ScrollView>
+            <View style={{ marginTop: top + 20 }}>
+                {/* Carrousel principal */}
+                <View style={{ height: 440, }} >
+                    <Carousel
+                        data={peliculasEnCine}
+                        renderItem={({ item }: any) => <MoviePoster movie={item} />}
+                        sliderWidth={windowWidth}
+                        itemWidth={300}
+                    />
+                </View>
+
+                {/* Peliculas populares */}
+                <View style={{ backgroundColor: 'red', height: 260 }}>
+                    <Text style={{ fontSize: 30, fontWeight: 'bold', color: 'white', marginHorizontal: 20 }}>
+                        En cine
+                    </Text>
+                    <FlatList
+                        data={peliculasEnCine}
+                        renderItem={({ item }: any) => (
+                            <MoviePoster movie={item} height={200} width={140} />
+                        )}
+                        keyExtractor={(item) => item.id.toString()}
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                    >
+
+                    </FlatList>
+                </View>
+
             </View>
-        </View>
+        </ScrollView>
+
     )
 }
