dfb6953f4b0e252a8d96a295795a0d1950bdf73a
krishee107 <krishee107@gmail.com>
2023-04-27 19:08:48 +0200

    Mostrar poster y carousel

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,18 @@
 
 import React from 'react'
 import { ActivityIndicator, Text, View } from 'react-native'
+import Carousel from 'react-native-snap-carousel';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import { useMovies } from '../hooks/useMovies'
 import { Loading } from '../components/Loading';
+import { MoviePoster } from '../components/MoviePoster';
+import { Dimensions } from 'react-native';
 
 export const HomeScreen = () => {
     const { peliculasEnCine, isLoading } = useMovies();
+    const { top } = useSafeAreaInsets();
+    const { width: windowWidth } = Dimensions.get('window');
 
     if (isLoading) {
         return (
@@ -17,8 +24,15 @@ export const HomeScreen = () => {
 
 
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={{ marginTop: top + 20 }}>
+            <View style={{ height: 440, }} >
+                <Carousel
+                    data={peliculasEnCine}
+                    renderItem={({ item }: any) => <MoviePoster movie={item} />}
+                    sliderWidth={windowWidth}
+                    itemWidth={300}
+                />
+            </View>
         </View>
     )
 }
