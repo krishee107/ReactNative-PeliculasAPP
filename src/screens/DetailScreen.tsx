dd71e8aa6d555051061848bf4fc888a2d361634b
krishee107 <krishee107@gmail.com>
2023-04-27 21:01:33 +0200

    Inicio de diseño de la screen Details

--- src/screens/DetailScreen.tsx
+++ src/screens/DetailScreen.tsx
@@ -1,18 +1,69 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, StyleSheet, Dimensions } from 'react-native';
 import { Movie } from '../interfaces/movieInterface'
 import { StackScreenProps } from '@react-navigation/stack'
 import { RootStackParams } from '../navigation/Navigation'
+import { ScrollView } from 'react-native-gesture-handler';
 
 interface Props extends StackScreenProps<RootStackParams, 'DetailScreen'> { // any, any = Params, Route
     movie: Movie
 }
+
+const screenHeight = Dimensions.get('screen').height;
 export const DetailScreen = ({ route }: Props) => {
     const movie = route.params;
+    const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
 
     return (
-        <View>
-            <Text>{movie.title}</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={{ uri }}
+                    style={styles.posterImage}
+                />
+            </View>
+
+            <View>
+                <View style={styles.marginContainer} >
+                    <Text style={styles.subtitle} >{movie.original_title}</Text>
+                    <Text style={styles.title} >{movie.title}</Text>
+                </View>
+            </View>
+        </ScrollView >
+
     )
 }
+
+const styles = StyleSheet.create({
+    imageContainer: {
+        width: '100%',
+        height: screenHeight * 0.7,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 10,
+        },
+        shadowOpacity: 0.24,
+        shadowRadius: 7,
+        elevation: 9,
+        borderBottomEndRadius: 25,
+        borderBottomStartRadius: 25,
+        overflow: 'hidden',
+    },
+    posterImage: {
+        flex: 1,
+    },
+    marginContainer: {
+        marginHorizontal: 20,
+        marginTop: 20,
+    },
+    subtitle: {
+        fontSize: 16,
+        opacity: 0.8,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
+})
